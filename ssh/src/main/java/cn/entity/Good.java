1bab980076fed48d62d6bf84f0b18265b4603ed6
wq <wq@DESKTOP-4VHRPTG>
2019-05-11 10:36:34 +0800

    ssh整合

---
+++ ssh/src/main/java/cn/entity/Good.java
@@ -0,0 +1,32 @@
+package cn.entity;
+
+public class Good {
+private int mid;
+private String namee;
+private String authore;
+private int typee;
+public int getMid() {
+	return mid;
+}
+public void setMid(int mid) {
+	this.mid = mid;
+}
+public String getNamee() {
+	return namee;
+}
+public void setNamee(String namee) {
+	this.namee = namee;
+}
+public String getAuthore() {
+	return authore;
+}
+public void setAuthore(String authore) {
+	this.authore = authore;
+}
+public int getTypee() {
+	return typee;
+}
+public void setTypee(int typee) {
+	this.typee = typee;
+}
+}
