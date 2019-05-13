2a835a176ff0044100ab44be9af0ca0a5cd35491
wq <wq@DESKTOP-4VHRPTG>
2019-05-13 20:59:04 +0800

    实现数据的增 删 改 查操作

--- ssh/src/main/java/cn/test/Test01.java
+++ ssh/src/main/java/cn/test/Test01.java
@@ -1,5 +1,8 @@
 package cn.test;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
@@ -20,8 +23,21 @@ public static void main(String[] args) {
 //Good g=(Good) session.get(Good.class, 5);
 //System.out.println(g.toString());
 	
+	//条件查询
+//	String HQL="from Good where mid<? and mid>?";
+//	Query qr=session.createQuery(HQL);
+//	qr.setInteger(0,10);
+//	qr.setInteger(1, 2);
+//	List<Good> list=qr.list();
+//	System.out.println(list);
 	
-	
+	//分页查询
+//	String HQL="from Good";
+//	Query qr=session.createQuery(HQL);
+//	qr.setFirstResult(0);//查询前面五条数据
+//	qr.setMaxResults(5);
+//	List<Good> list=qr.list();
+//	System.out.println(list);
 	
 	//对于数据的增 删 改操作需要提交事务=>通过session得到事务
 //Transaction tx=session.beginTransaction();
